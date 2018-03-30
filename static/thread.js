ea7063edc9b5304ecc5ead3d74713cc397c94fc7
Alex Fridman <alfrid12@gmail.com>
2018-03-30 09:46:28 -0400

    Updated threat.html and thread.js

--- static/thread.js
+++ static/thread.js
@@ -3,7 +3,13 @@ console.log("So far so good");
 $(document).ready(function(){
 
 	for(let i = 0; i < 3; i++){
-		document.body.innerHTML += '<div class = "comment_container" style = "width: 100%; height: 10%; border: 1px solid black; box-sizing: border-box; margin: 2% 0 0 0;"><div style = "width: 15%; height: 100%; border: 1px solid blue; display: inline-block;"><div style = "width: 40%; height: 100%; display: inline-block; font-size: 250%;">166&nbsp;</div><div style = "width: 40%; height: 100%; display: inline-block;"><input type = "button" style = "width: 90%; height: 50%;" value = "Upvote"><input type = "button" style = "width: 90%; height: 50%;" value = "Downvote"></div></div><div style = "width: 84%; height: 100%; border: 1px solid red; display: inline-block; vertical-align: top;">This is random garbage text. It doesn\'t have any value or purpose; it exists solely as a placeholder.</div></div>'
+		var comment_html = '';
+		comment_html += '<div class = "comment_container"><div class = "vote_container">';
+		comment_html += '<div class = "vote_count_container">166&nbsp;</div><div class = "vote_buttons_container">';
+		comment_html += '<input type = "button" class = "vote_button" value = "Upvote">';
+		comment_html += '<input type = "button" class = "vote_button" value = "Downvote">';
+		comment_html += '</div></div><div class = "comment_text_container">This is random garbage text. It doesn\'t have any value or purpose; it exists solely as a placeholder.</div></div>';
+		document.body.innerHTML += comment_html;
 	}
 
 
